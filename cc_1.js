9ebca40d5d0b7dfa346d83751441e13fd368efb0
Your Name <your.email@example.com>
2025-01-23 15:29:36 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,11 @@ var isActive = true
 console.log("EmployeeName", employeeName, "Type of", typeof employeeName )
 console.log("EmployeeID",employeeID, "Type of", typeof employeeID )
 console.log("Is Active", isActive, "Type of", typeof isActive)
+
+let productName = "Sona"
+const productPrice = "300"
+var isAvailable = true
+
+console.log("ProductName", productName, "Type of", typeof productName)
+console.log("ProductPrice", productPrice, "Type of", typeof productPrice)
+console.log("Is Available", isAvailable, "Type of", typeof isAvailable)
