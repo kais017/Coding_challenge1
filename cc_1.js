953979f8a510261caedd1583dde05af9f3818028
Your Name <your.email@example.com>
2025-01-23 13:45:24 -0500

    Task 1 - Employee Info

--- cc_1.js
+++ cc_1.js
@@ -1,4 +1,8 @@
 
 let employeeName = "Kaileen Kennedy"
-const employeeID = 123
+const employeeID = 01
 var isActive = true
+
+console.log("EmployeeName", employeeName, "Type of", typeof employeeName )
+console.log("EmployeeID",employeeID, "Type of", typeof employeeID )
+console.log("Is Active", isActive, "Type of", typeof isActive)
