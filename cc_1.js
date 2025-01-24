891be650ebd131bf5dbf6030835bd029d600e82f
Your Name <your.email@example.com>
2025-01-24 16:13:41 -0500

    Edited Task 3

--- cc_1.js
+++ cc_1.js
@@ -16,16 +16,28 @@ console.log("ProductPrice", productPrice, "Type of", typeof productPrice)
 console.log("Is Available", isAvailable, "Type of", typeof isAvailable)
 
 let accountBalance = 50000
-let NewaccountBalance = accountBalance + 300
+console.log("AccountBalance $", accountBalance, "Type of", typeof accountBalance)
 
-console.log("AccountBalance", accountBalance, "Type of", typeof accountBalance)
-console.log("After deposit: $" + NewaccountBalance)
+accountBalance += 300
+console.log("After addition: $", accountBalance)
+
+accountBalance -= 1000 
+console.log("After subtraction: $", accountBalance)
+
+accountBalance *= 2
+console.log("After multiplication: $", accountBalance)
+
+accountBalance /= 5
+console.log("After division: $", accountBalance)
+
+
 
 let customerName = "Batman"
 let welcomeMessage = "Hey there, " + customerName + "! Welcome to Gotham City!"
 console.log(welcomeMessage)
 
 let isLoggedIn = true
+
 let age = 100
 let isMember = true
 if (age >= 100 && isMember)
